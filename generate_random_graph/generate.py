557ac9489a36183922b4954c5d53d5f38d701285
Ken Park <sunghopark2010@gmail.com>
2014-06-04 11:05:27 -0500

    modified the script to generate a random graph

--- generate_random_graph/generate.py
+++ generate_random_graph/generate.py
@@ -1,23 +1,31 @@
-# API: python generate.py <path of output file> <number of nodes> <probability of edges> <probability of fraud>
-# <probability of edges> is defined as the number of nodes that will be linked to this node / the number of all pre-existing nodes
+# API: python generate.py <number of nodes> <probability of edges>
 
 import sys
 import random
+import networkx as nx
 
 if __name__ == '__main__':
-	output_path = sys.argv[1]
-	num_nodes = int(sys.argv[2])
-	prob_edges = float(sys.argv[3])
-	prob_fraud = float(sys.argv[4])
+	num_nodes = int(sys.argv[1])
+	prob_edges = float(sys.argv[2])
 
-	f = open(output_path, 'w')
-	for i in range(1, num_nodes+1):
-		fraud_flg = True if random.random() <= prob_fraud else False
-		neighbors = list()
-		for j in range(1, i):
-			if random.random() <= prob_edges:
-				neighbors.append(str(j))
-		f.write('%d|%s|%s\n' % (i, fraud_flg, ','.join(neighbors)))
+	output_path = 'graph_%d_%4.2f.data' % (num_nodes, prob_edges)
+	f1 = open('bfs_' + output_path, 'w')
+	f2 = open('fw_' + output_path, 'w')
 
-		if i % 10000 == 0:
-			print i
+	G = nx.gnp_random_graph(num_nodes, prob_edges)
+
+	for node in G.nodes():
+		f1.write('%d\t%s|Integer.MAX_VALUE|WHITE|null\n' % (node, ','.join(map(lambda n: str(n), G.neighbors(node)))))
+
+		for end_node in G.nodes():
+			if end_node in G.neighbors(node):
+				f2.write('%d,%d,%d\n' % (node, end_node, 1))
+			else:
+				f2.write('%d,%d,Integer.MAX_VALUE\n' % (node, end_node))
+
+	f1.close()
+	f2.close()
+		
+		
+
+	
